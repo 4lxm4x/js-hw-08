2883a89c1ca5371fa9926c6a889cad86b724fdbf
Oleksandr Maksymenko <machinery69@gmail.com>
2023-10-22 23:21:15 +0300

    finished hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import _ from 'lodash';
 
+const formElement = document.querySelector('form');
+const emailElement = document.querySelector('input');
+const msgElement = document.querySelector('textarea');
+if (localStorage) {
+  const { email, message } = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+
+  emailElement.value = email;
+  msgElement.value = message;
+}
+formElement.addEventListener('input', _.throttle(onFormInput, 500));
+formElement.addEventListener('submit', onSubmit);
+
+function onFormInput() {
+  const data = new FormData(formElement);
+  const formData = {
+    email: data.get('email'),
+    message: data.get('message'),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  formElement.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.clear();
+}
